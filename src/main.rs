cb3a3bcb6385ceec32918b14f5386aa945af195a
Caio Campos <caio.campos1204@gmail.com>
2019-02-17 19:26:09 -0300

    Funções para configuração e busca de arquivos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use std::io;
 use rand::Rng;
+
+mod conf;
+mod file;
 
 fn main() {
     let max = 1000;
     let img_number = rand::thread_rng().gen_range(1, max);
+
+    let config = conf::load_conf();
+
+    println!("{:?}", config);
+
+    let list = file::list_images(&config.back_folder);
+    println!("{:?}", list);
+
+    println!("{}", img_number);
 }
